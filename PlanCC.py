dab6b5385ddbae92de860048f31571c4f09cf5e2
Sebatro <sebastian.trommer@gmail.com>
2021-01-06 21:09:54 +0100

    Main function done

--- PlanCC.py
+++ PlanCC.py
@@ -1,4 +1,5 @@
-import requests, pandas
+import requests, pandas, time
+import matplotlib.pyplot as plt
 
 # configuration
 strUrl = "https://api.awattar.at/v1/marketdata"
@@ -11,6 +12,7 @@ strPrice = "marketprice"
 startData = []
 endData = []
 priceData = []
+priceEval = []
 mean = 0
 
 r = requests.get(strUrl)
@@ -23,6 +25,27 @@ for d in range(len(r.json()[strData])):
     mean = mean + r.json()[strData][d][strPrice]
 
 mean = mean / len(r.json()[strData])    
+
+for p in range(len(priceData)):
+    if priceData[p] > mean:
+        priceEval.append('higher')
+    elif priceData[p] <= mean:
+        priceEval.append('lower')
+    else:
+        pass  
+
 print(priceData)
+print(priceEval)
 print(mean)
-#print(pandas.Series.mean(priceData))
+print(startData[0]*(10**6))
+print(time.time_ns())
+
+print(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(startData[0]*(10**-3))))
+   
+def GetDatafromAwattar(self):
+    # Get data from Awattar API
+    pass
+
+def WriteToInfluxDB(self, pstrTimestamp as float, strValue as str):
+    # Writes value to the influxDb using HTTP get method like described in https://docs.influxdata.com/influxdb/v1.8/guides/write_data/
+    pass
